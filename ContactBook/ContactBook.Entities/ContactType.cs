6b2691ed592ba0b499d1f1e6190ccb1dab22e87a
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2023-12-16 11:17:25 -0300

    create the model ContactType and writing the UnitTest

---
+++ ContactBook/ContactBook.Entities/ContactType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactBook.Entities
+{
+    public class ContactType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ ContactBook/ContactBookApi.Tests/01-Entities/ContactTypeUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using ContactBook.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactBookApi.Tests._01_Entities
+{
+    public class ContactTypeUnitTest
+    {
+
+        [Theory]
+        [InlineData(1, "Test", true)]
+        [InlineData(2, "Email", true)]
+        [InlineData(3, "Phone", true)]
+        [InlineData(4, "Whatsapp", true)]
+        [InlineData(5, "This text will fail because it's too long. This one can't pass never!!!!!!!", false)]
+        public void ValidateModel(int id, string description, bool expected)
+        {
+            //Assemble
+            ContactType contactType = DumpEntity(id, description);
+
+            //Act
+            var validationResults = new List<ValidationResult>();
+            var actual = Validator.TryValidateObject(contactType, 
+                new ValidationContext(contactType), 
+                validationResults, 
+                validateAllProperties: true);
+
+            //Assert
+            Assert.Equal(actual, expected);
+        }
+
+        private static ContactType DumpEntity(int id, string description)
+        {
+            return new ContactType()
+            {
+                Id = id,
+                Description = description,
+                CreatedAt = DateTime.Now
+            };
+        }
+    }
+}
